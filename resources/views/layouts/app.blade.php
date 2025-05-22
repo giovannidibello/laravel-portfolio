ad08d614f1fbb687c49498b571d077db02460d04
Giovanni <dibellogio@gmail.com>
2025-05-22 17:24:49 +0200

    fix style welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+    @vite(["resources/sass/app.scss" , "resources/js/app.js" ])
 </head>
 
 <body>
