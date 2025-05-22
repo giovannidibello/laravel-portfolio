0528d9fdc2c3083f774997e5907f6b534146f5be
Giovanni <dibellogio@gmail.com>
2025-05-22 16:54:39 +0200

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // utilizzo il seeder per la tabella Projects
+        $this->call([ProjectsTableSeeder::class]);
     }
 }
