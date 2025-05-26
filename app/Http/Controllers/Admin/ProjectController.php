2359911791b13c2e1ac7583a7765b542cba3f69f
Giovanni <dibellogio@gmail.com>
2025-05-26 16:53:40 +0200

    add destroy route

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\New_;
 
 class ProjectController extends Controller
 {
@@ -23,7 +24,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view("project.create");
     }
 
     /**
@@ -31,7 +32,18 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newProject = new Project();
+
+        $newProject->name = $data["name"];
+        $newProject->period = $data["period"];
+        $newProject->customer = $data["customer"];
+        $newProject->summary = $data["summary"];
+
+        $newProject->save();
+
+        return redirect()->route("project.show", $newProject);
     }
 
     /**
@@ -46,24 +58,37 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project)
     {
-        //
+        // dd($project);
+        return view("project.edit", compact("project"));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Project $project)
     {
-        //
+        $data = $request->all();
+
+        // modifico le informazioni del progetto
+        $project->name = $data["name"];
+        $project->period = $data["period"];
+        $project->customer = $data["customer"];
+        $project->summary = $data["summary"];
+
+        $project->update();
+
+        return redirect()->route("project.show", $project);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route("project.index");
     }
 }
